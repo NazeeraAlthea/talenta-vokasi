f1801f5cc0ebea6a12fcb42e44232ae53aac4417
NazeeraAlthea <ryamaulana2894@gmail.com>
2025-07-07 13:04:14 +0700

    logo bug

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from 'next';
 const config: NextConfig = {
   images: {
     remotePatterns: [
-      // 1. Konfigurasi untuk Supabase LOKAL (saat 'npm run dev')
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '54321',
-        pathname: '/storage/v1/object/public/**',
-      },
-      // 2. Konfigurasi untuk Supabase PRODUKSI (saat di Netlify)
       {
         protocol: 'https',
         hostname: 'https://maasegyxpoqxdllnwapd.supabase.co',
